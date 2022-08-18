98572f185bbb7dc42a112cf3b85e66e4f407aadc
danthor11 <danieljpb11@gmail.com>
2022-08-18 19:37:56 -0400

    componente mylabel listo para usar

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import "./myLabel.css"
 
-export const MyLabel = () => {
+interface Props {
+  /**
+   *  This is the text that contain the component
+   */
+  label: string,
+
+  /**
+   * This is the size of the component
+   */
+  size: "normal" | "h1" | "h2" | "h3",
+  /**
+   * if you want all text capitalized
+   */
+  allCaps?: boolean,
+  /**
+   * Basic colors
+   */
+  color?: "primary" | "secondary" | "tertiary"
+  /**
+   * Custom font's color
+   */
+
+  fontColor?: string
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color = "primary",
+  fontColor,
+  label = "No Label",
+  size = "normal"
+}: Props) => {
   return (
-    <span>
-        Hola mundo
+    <span 
+      className={`label ${size} text-${color}`}
+      style={{color:fontColor}}
+    >
+      {allCaps ? label.toUpperCase() : label}
     </span>
   )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,15 +1,50 @@
-import { MyLabel } from "../../components/MyLabel";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MyLabel} from "../../components/MyLabel";
 
 
 export default {
-    title:"Example/My Label",
-    component: MyLabel
+    title:"UI/My Label",
+    component: MyLabel,
+    argTypes:{
+        size:{control:"select"},
+        color:{control:"select"},
+        fontColor:{control:"color"}
+    }
+} as ComponentMeta<typeof MyLabel>
+
+
+const Template : ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/>
+
+export const Basic = Template.bind({})
+
+Basic.args = {
+    size:"normal",
+    allCaps:false // true: Capitalizar toda la palabra
+}
+
+export const AllCaps = Template.bind({})
+AllCaps.args={
+    size:"normal",
+    allCaps:true
+}
+export const Secondary = Template.bind({})
+
+Secondary.args = {
+    size:"normal",
+    color:"secondary"
+}
+
+export const Tertiary = Template.bind({})
+
+Tertiary.args = {
+    size:"normal",
+    color:"tertiary"
 }
 
 
-const Template = () => <MyLabel />
+export const CustomFontColor = Template.bind({})
 
-export const Basic = Template.bind({})
-export const AllCaps = Template.bind({})
-export const Secondary = Template.bind({})
-
+CustomFontColor.args = {
+    size:"h1",
+    fontColor: "#202020"
+}
